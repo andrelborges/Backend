3e618ed4a26020b1dcf4396d9259f5ad87093ce0
Andree Borges <andreborges118@hotmail.com>
2023-04-07 02:12:15 -0300

    adicionado modulo de for

---
+++ C#_loop_for/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+
+for (int i = 0; i< 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n");
+
+for (int j = 10; j >= 0; j--)
+{
+    Console.WriteLine(j);
+}
+
+Console.WriteLine("\n");
+
+//Variavel Local
+for (int i = 0; i < 10; i +=3)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+*/
+
+string[] names = {"Alex", "Eddie", "David", "Michael"};
+
+for (int i = names.Length -1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0 ; i < 4; i++)
+{
+    Console.WriteLine(names[i] + i);
+    if (names[i] == "David") names[i] = "Sammy";
+}
+
+Console.WriteLine("\n");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+    
+}
+
+//FIzzBuzz
+
+for (int i = 1; i <= 20; i++)
+{
+    //Console.WriteLine(i);
+    if( i % 3 == 0 && i % 5 == 0 )
+        {
+            Console.WriteLine($"{i} - FizzBuzz");
+        }
+    else if (i % 3 == 0) 
+        {
+            Console.WriteLine($"{i} - Fizz");
+        }
+    else if (i % 5 == 0)
+        {
+            Console.WriteLine($"{i} - Buzz");
+        } else 
+        {
+            Console.WriteLine(i);
+        }
+}
---
+++ Desafios_em_C#/arrays_iteration_selection/Program.cs
@@ -0,0 +1,87 @@
+﻿/* 
+
+
+    Create a C# console application.
+
+    Start with four students. Each student has five exam scores.
+
+    Each exam score is an integer value, 0-100, where 100 represents 100% correct.
+
+    A student's overall exam score is the average of their five exam scores.
+
+    Criteria for extra credit assignments:
+        Include extra credit assignment scores in the student's scores array.
+        Extra credit assignments are worth 10% of an exam score (when calculating the final numeric grade).
+        Add extra credit assignment scores to the student's total exam score before calculating the final numeric grade.
+
+    Your application needs to automatically assign letter grades based on the calculated final score for each student.
+
+    Your application needs to output/display each student’s name and formatted grade.
+
+    Your application needs to support adding other students and scores with minimal impact to the code.
+*/
+
+//Adicionando 4 estudantes 
+
+string student1 = "Andre";
+
+string student2 = "Bruno";
+string student3 = "Carlos";
+string tudent4 = "Diogo";
+
+int currentAssignments = 5;
+
+int sophia1 = 90;
+int sophia2 = 86;
+int sophia3 = 87;
+int sophia4 = 98;
+int sophia5 = 100;
+
+int andrew1 = 92;
+int andrew2 = 89;
+int andrew3 = 81;
+int andrew4 = 96;
+int andrew5 = 90;
+
+int emma1 = 90;
+int emma2 = 85;
+int emma3 = 87;
+int emma4 = 98;
+int emma5 = 68;
+
+int logan1 = 90;
+int logan2 = 95;
+int logan3 = 87;
+int logan4 = 88;
+int logan5 = 96;
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
+
+
+sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
+emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+andrewScore = (decimal)andrewSum / currentAssignments;
+emmaScore = (decimal)emmaSum / currentAssignments;
+loganScore = (decimal)loganSum / currentAssignments;
+
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
